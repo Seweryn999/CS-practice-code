5561f54bb33fcd803a0bd5eb3057e7c7d852da6e
Seweryn <150341055+Seweryn999@users.noreply.github.com>
2024-11-17 11:30:47 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class TestDwa
+{
+
+    static void budget()
+    {
+        Console.WriteLine("-----Wybierz opcje-----");
+        Console.WriteLine("1. Podaj przychód");
+        Console.WriteLine("2. Podaj wydatki");
+        Console.WriteLine("3. Pokaż rozliczenie");
+        Console.WriteLine("Wybierz opcje (1, 2, 3): ");
+        int op = Convert.ToInt32(Console.Read());
+
+        
+    static void przychod()
+    {
+if(op == 1)
+{
+
+}
+    }
+}
+   
+ 
+}
